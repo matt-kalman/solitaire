991da1af6e351adea07bc6a7fe35bbc61987d895
DeathHotDog <matthewkalman1@gmail.com>
2020-01-01 19:38:03 -0500

    Initial commit

---
+++ cards.h
@@ -0,0 +1,86 @@
+#ifndef CARDS_H
+#define CARDS_H
+
+#include <vector>
+#include <SDL.h>
+#include "images.h"
+
+using namespace std;
+
+SDL_Renderer* renderer;
+SDL_Surface* image;
+SDL_Texture* texture;
+int cw = 107, ch = 144;
+
+SDL_Surface* surface(int x, int w, int h, int a) {
+	Uint32 rmask, gmask, bmask, amask;
+	if (SDL_BYTEORDER == SDL_BIG_ENDIAN) {
+		int shift = (a == 3) ? 8 : 0;
+		rmask = 0xff000000 >> shift;
+		gmask = 0x00ff0000 >> shift;
+		bmask = 0x0000ff00 >> shift;
+		amask = 0x000000ff >> shift;
+	} else {
+		rmask = 0x000000ff;
+		gmask = 0x0000ff00;
+		bmask = 0x00ff0000;
+		amask = (a == 3) ? 0 : 0xff000000;
+	}
+
+	return SDL_CreateRGBSurfaceFrom((void*)img[x], w, h, 8 * a, a * w, rmask, gmask, bmask, amask);
+}
+
+struct Card {
+    int rank, suit;
+    bool faceUp = false, color = false;
+    SDL_Rect rect = {0,0,cw,ch};
+
+    Card(int r, int s) {
+        rank = r;
+        suit = s;
+
+        if (suit == 0 || suit == 3) {
+            color = true;
+        }
+    }
+
+    Card(const Card &other) {
+        memcpy(this, &other, sizeof(Card));
+    }
+
+    void flip() {
+        faceUp = !faceUp;
+    }
+
+    void show() {
+        if (!faceUp) {
+            image = surface(52, 71, 96, 3);
+        } else {
+            image = surface(suit*13 + rank, 71, 96, 3);
+        }
+
+        texture = SDL_CreateTextureFromSurface(renderer, image);
+        SDL_FreeSurface(image);
+        image = NULL;
+
+        SDL_RenderCopy(renderer, texture, 0, &rect);
+        SDL_DestroyTexture(texture);
+        texture = NULL;
+    }
+};
+
+struct Pile {
+    vector<Card*> cards;
+    SDL_Rect rect = {0,0,cw,ch};
+
+    void add(Card* card) {
+        cards.push_back(card);
+    }
+
+    void give(Card* card, Pile &other) {
+        cards.erase(remove(begin(cards), end(cards), card), end(cards));
+        other.add(card);
+    }
+};
+
+#endif
---
+++ images.h
@@ -0,0 +1,72 @@
+#include "images/s01.c"
+#include "images/s02.c"
+#include "images/s03.c"
+#include "images/s04.c"
+#include "images/s05.c"
+#include "images/s06.c"
+#include "images/s07.c"
+#include "images/s08.c"
+#include "images/s09.c"
+#include "images/s10.c"
+#include "images/s11.c"
+#include "images/s12.c"
+#include "images/s13.c"
+#include "images/h01.c"
+#include "images/h02.c"
+#include "images/h03.c"
+#include "images/h04.c"
+#include "images/h05.c"
+#include "images/h06.c"
+#include "images/h07.c"
+#include "images/h08.c"
+#include "images/h09.c"
+#include "images/h10.c"
+#include "images/h11.c"
+#include "images/h12.c"
+#include "images/h13.c"
+#include "images/d01.c"
+#include "images/d02.c"
+#include "images/d03.c"
+#include "images/d04.c"
+#include "images/d05.c"
+#include "images/d06.c"
+#include "images/d07.c"
+#include "images/d08.c"
+#include "images/d09.c"
+#include "images/d10.c"
+#include "images/d11.c"
+#include "images/d12.c"
+#include "images/d13.c"
+#include "images/c01.c"
+#include "images/c02.c"
+#include "images/c03.c"
+#include "images/c04.c"
+#include "images/c05.c"
+#include "images/c06.c"
+#include "images/c07.c"
+#include "images/c08.c"
+#include "images/c09.c"
+#include "images/c10.c"
+#include "images/c11.c"
+#include "images/c12.c"
+#include "images/c13.c"
+#include "images/back.c"
+#include "images/icon.c"
+#include "images/res.c"
+#include "images/und.c"
+#include "images/solv.c"
+
+const unsigned char* img[57] = {
+    s01.pixel_data, s02.pixel_data, s03.pixel_data, s04.pixel_data, s05.pixel_data,
+    s06.pixel_data, s07.pixel_data, s08.pixel_data, s09.pixel_data, s10.pixel_data,
+    s11.pixel_data, s12.pixel_data, s13.pixel_data, h01.pixel_data, h02.pixel_data,
+    h03.pixel_data, h04.pixel_data, h05.pixel_data, h06.pixel_data, h07.pixel_data,
+    h08.pixel_data, h09.pixel_data, h10.pixel_data, h11.pixel_data, h12.pixel_data,
+    h13.pixel_data, d01.pixel_data, d02.pixel_data, d03.pixel_data, d04.pixel_data,
+    d05.pixel_data, d06.pixel_data, d07.pixel_data, d08.pixel_data, d09.pixel_data,
+    d10.pixel_data, d11.pixel_data, d12.pixel_data, d13.pixel_data, c01.pixel_data,
+    c02.pixel_data, c03.pixel_data, c04.pixel_data, c05.pixel_data, c06.pixel_data,
+    c07.pixel_data, c08.pixel_data, c09.pixel_data, c10.pixel_data, c11.pixel_data,
+    c12.pixel_data, c13.pixel_data,	back.pixel_data, icon.pixel_data, res.pixel_data,
+    und.pixel_data, solv.pixel_data
+};
